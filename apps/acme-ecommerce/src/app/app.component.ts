ff5233bd1eafa7dca7fcbe65e838d54de5a01df5
magdyshata <magdyshata35@gmail.com>
2025-09-24 11:52:03 +0300

    add-14

--- apps/acme-ecommerce/src/app/app.component.ts
+++ apps/acme-ecommerce/src/app/app.component.ts
@@ -4,12 +4,20 @@ import { NxWelcomeComponent } from './nx-welcome.component';
 import { NavComponent } from './nav/nav.component';
 import { Store } from '@ngrx/store';
 import { selectProducts, selectLoading } from './products/products.selectors';
-import { AsyncPipe, CommonModule } from '@angular/common';
+import { AsyncPipe, CommonModule, NgFor, NgIf } from '@angular/common';
 import * as ProductsActions from './products/products.actions';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, NavComponent, AsyncPipe,CommonModule],
+  imports: [
+    NxWelcomeComponent,
+    RouterModule,
+    NavComponent,
+    AsyncPipe,
+    CommonModule,
+    NgIf,
+    NgFor,
+  ],
   selector: 'acme-ecommerce-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
@@ -23,13 +31,12 @@ export class AppComponent {
 
   ngOnInit() {
     this.store.dispatch(ProductsActions.loadProducts());
-     // Debug
-  this.products$.subscribe(products => {
-    console.log('Products:', products);
-  });
-  this.loading$.subscribe(loading => {
-    console.log('Loading:', loading);
-  });
+    // Debug
+    // this.products$.subscribe((products) => {
+    //   console.log('Products:', products);
+    // });
+    // this.loading$.subscribe((loading) => {
+    //   console.log('Loading:', loading);
+    // });
   }
-
 }
