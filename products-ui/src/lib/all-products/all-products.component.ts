bab660df54cf45cba6a66bb3f1e78f9ff9961812
magdyshata <magdyshata35@gmail.com>
2025-09-25 13:13:01 +0300

    add-32

--- products-ui/src/lib/all-products/all-products.component.ts
+++ products-ui/src/lib/all-products/all-products.component.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 import { Store } from '@ngrx/store';
@@ -8,6 +8,7 @@ import {
   selectProducts,
   selectLoading,
 } from 'products/src/products/products.selectors';
+import * as ProductsActions from 'products/src/products/products.actions';
 @Component({
   selector: 'acme-ecommerce-all-products',
   standalone: true,
@@ -15,10 +16,24 @@ import {
   templateUrl: './all-products.component.html',
   styleUrls: ['./all-products.component.scss'],
 })
-export class AllProductsComponent {
+export class AllProductsComponent implements OnInit {
   private route = inject(ActivatedRoute);
   private store = inject(Store);
   category = 'All';
   products$ = this.store.select(selectProducts);
   loading$ = this.store.select(selectLoading);
+
+  ngOnInit() {
+    // Get category from URL (e.g., /categories/electronics → 'electronics')
+    this.category = this.route.snapshot.paramMap.get('category') || 'All';
+
+    // ✅ Dispatch the correct action based on category
+    if (this.category === 'All') {
+      this.store.dispatch(ProductsActions.loadProducts());
+    } else {
+      this.store.dispatch(
+        ProductsActions.loadProductsByCategory({ category: this.category })
+      );
+    }
+  }
 }
